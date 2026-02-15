5cd35b4eef3ed049bbda9dd9deaf0efbcb65593b
Nathan Hansle <nathanhansle16@gmail.com>
2026-02-15 18:50:19 -0500

    Added an image and styled the webpage.

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 # Routing for your application.
 # Put your routes below this comment
 '''
+@app.route('/')
+def home():
+    return render_template('about.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
